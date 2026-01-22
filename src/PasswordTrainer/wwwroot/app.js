d9ed62684096f60d93d97af931c95652984879cf
mu88 <4560672+mu88@users.noreply.github.com>
2026-01-22 16:57:53 +0100

    chore: fix Sonar issues

--- src/PasswordTrainer/wwwroot/app.js
+++ src/PasswordTrainer/wwwroot/app.js
@@ -1,22 +1,21 @@
 const form = document.getElementById("form");
 const result = document.getElementById("result");
 
+function toBase64(bytes) {
+  let binary = '';
+  for (let b of bytes) binary += String.fromCodePoint(b);
+  return btoa(binary);
+}
+
 form.addEventListener("submit", async e => {
   e.preventDefault();
   result.textContent = "";
-
 
   const encoder = new TextEncoder();
   const pin = document.getElementById("pin").value.trim();
   const id = document.getElementById("id").value.trim();
   const password = document.getElementById("pw").value.trim();
 
-  // Encode password as UTF-8 bytes, then base64
-  function toBase64(bytes) {
-    let binary = '';
-    for (let b of bytes) binary += String.fromCharCode(b);
-    return btoa(binary);
-  }
   const passwordBytes = encoder.encode(password);
   const passwordBase64 = toBase64(passwordBytes);
 
